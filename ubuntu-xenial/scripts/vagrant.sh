57c1102ddbbc282c1f1192b12229f3095fbb92e2
Alex Shepherd <gr4unch3r@protonmail.com>
2022-05-26 11:33:06 +0100

    Update vagrant.sh

--- ubuntu-xenial/scripts/vagrant.sh
+++ ubuntu-xenial/scripts/vagrant.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eux
 
 pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub";
-mkdir -p $HOME_DIR/.ssh;
+mkdir -p /home/vagrant/.ssh;
 if command -v wget >/dev/null 2>&1; then
-    wget --no-check-certificate "$pubkey_url" -O $HOME_DIR/.ssh/authorized_keys;
+    wget --no-check-certificate "$pubkey_url" -O /home/vagrant/.ssh/authorized_keys;
 elif command -v curl >/dev/null 2>&1; then
-    curl --insecure --location "$pubkey_url" > $HOME_DIR/.ssh/authorized_keys;
+    curl --insecure --location "$pubkey_url" > /home/vagrant/.ssh/authorized_keys;
 else
     echo "Cannot download vagrant public key";
     exit 1;
 fi
-chown -R vagrant $HOME_DIR/.ssh;
-chmod -R go-rwsx $HOME_DIR/.ssh;
+chown -R vagrant /home/vagrant/.ssh;
+chmod -R go-rwsx /home/vagrant/.ssh;
